4ef5c304a989c9d6f5cc2b56e57919a9bd897965
Matt Parker <matt@mpcontracting.co.uk>
2017-11-17 22:55:45 +0000

    Initialisation and shutdown working again

--- rpmjukebox/src/main/java/uk/co/mpcontracting/rpmjukebox/RpmJukebox.java
+++ rpmjukebox/src/main/java/uk/co/mpcontracting/rpmjukebox/RpmJukebox.java
@@ -4,14 +4,21 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.Collections;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import de.felixroske.jfxsupport.AbstractJavaFxApplicationSupport;
 import de.felixroske.jfxsupport.SplashScreen;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import uk.co.mpcontracting.rpmjukebox.manager.ApplicationManager;
 import uk.co.mpcontracting.rpmjukebox.support.Constants;
 import uk.co.mpcontracting.rpmjukebox.view.MainPanelView;
 
@@ -20,37 +27,28 @@ import uk.co.mpcontracting.rpmjukebox.view.MainPanelView;
 public class RpmJukebox extends AbstractJavaFxApplicationSupport implements Constants {
 
 	@Getter private static File configDirectory;
+	
+	private ApplicationContext context;
 
-	public static void main(String[] args) {
-		// Look for the config directory and create it if it isn't there
-		File homeDir = new File(System.getProperty("user.home"));
+	@Override
+	public Collection<Image> loadDefaultIcons() {
+		return Collections.emptyList();
+	}
+	
+	@Override
+	public void beforeInitialView(Stage stage, ConfigurableApplicationContext context) {
+		this.context = context;
 		
-		// See if we have a command line override
-		if (System.getProperty("directory.config") != null) {
-			configDirectory = new File(homeDir, System.getProperty("directory.config"));
-			
-		} else {
-			configDirectory = new File(homeDir, ".rpmjukebox");
-		}
-
-		if (!configDirectory.exists()) {
-			if (!configDirectory.mkdirs()) {
-				throw new RuntimeException("Unable to create config directory - " + configDirectory.getAbsolutePath());
-			}
-		}
-
-		log.info("Config directory - " + configDirectory);
-
-		// Initialise the logging
-		initialiseLogging();
-
-		// Add a spacer to the logging file to separate startups
-		/*log.info("====================================================");
-		log.info("====================================================");
-		log.info("====================================================");
-		log.info("Java Version - " + System.getProperty("java.version"));*/
+		context.getBean(ApplicationManager.class).start(stage);
+	}
+	
+	@Override
+	public void stop() throws Exception {
+		context.getBean(ApplicationManager.class).stop();
 		
-		launchApp(RpmJukebox.class, MainPanelView.class, new RpmJukeboxSplash(), args);
+		super.stop();
+		
+		System.exit(0);
 	}
 	
 	private static void initialiseLogging() {
@@ -84,6 +82,41 @@ public class RpmJukebox extends AbstractJavaFxApplicationSupport implements Cons
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static void main(String[] args) {
+		// Look for the config directory and create it if it isn't there
+		File homeDir = new File(System.getProperty("user.home"));
+		
+		// See if we have a command line override
+		if (System.getProperty("directory.config") != null) {
+			configDirectory = new File(homeDir, System.getProperty("directory.config"));
+			
+		} else {
+			configDirectory = new File(homeDir, ".rpmjukebox");
+		}
+
+		if (!configDirectory.exists()) {
+			if (!configDirectory.mkdirs()) {
+				throw new RuntimeException("Unable to create config directory - " + configDirectory.getAbsolutePath());
+			}
+		}
+
+		log.info("Config directory - " + configDirectory);
+
+		// Initialise the logging
+		initialiseLogging();
+
+		// Add a spacer to the logging file to separate startups
+		/*log.info("====================================================");
+		log.info("====================================================");
+		log.info("====================================================");
+		log.info("Java Version - " + System.getProperty("java.version"));*/
+		
+		launchApp(RpmJukebox.class, MainPanelView.class, new RpmJukeboxSplash(), args);
+		
+		// Request the focus of the application
+		getStage().getScene().getRoot().requestFocus();
 	}
 
 	private static class RpmJukeboxSplash extends SplashScreen {
---
+++ rpmjukebox/src/main/java/uk/co/mpcontracting/rpmjukebox/manager/ApplicationManager.java
@@ -0,0 +1,89 @@
+package uk.co.mpcontracting.rpmjukebox.manager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
+import uk.co.mpcontracting.rpmjukebox.controller.MainPanelController;
+import uk.co.mpcontracting.rpmjukebox.event.Event;
+import uk.co.mpcontracting.rpmjukebox.event.EventManager;
+import uk.co.mpcontracting.rpmjukebox.jetty.JettyServer;
+import uk.co.mpcontracting.rpmjukebox.support.Constants;
+import uk.co.mpcontracting.rpmjukebox.support.OsType;
+import uk.co.mpcontracting.rpmjukebox.support.ThreadRunner;
+
+@Slf4j
+@Component
+public class ApplicationManager implements Constants {
+
+	@Autowired
+	private SettingsManager settingsManager;
+	
+	@Autowired
+	private SearchManager searchManager;
+	
+	@Autowired
+	private MainPanelController mainPanelController;
+	
+	@Autowired
+	private MediaManager mediaManager;
+	
+	@Autowired
+	private JettyServer jettyServer;
+	
+	private Stage stage;
+	private boolean isInitialised;
+	
+	public void start(Stage stage) {
+		log.info("Starting application");
+		
+		this.stage = stage;
+		
+		stage.setTitle("RPM Jukebox");
+
+		// If this is Windows, add a window icon
+		if (settingsManager.getOsType() == OsType.WINDOWS) {
+			stage.getIcons().add(new Image(getClass().getResourceAsStream(IMAGE_WINDOW_ICON)));
+		}
+		
+		// Load the window settings
+		settingsManager.loadWindowSettings(stage);
+
+		stage.show();
+		stage.requestFocus();
+
+		// Initialise data in a new thread for GUI responsiveness
+		ThreadRunner.run(() -> {
+			try {
+				searchManager.initialise();
+				settingsManager.loadSettings();
+				
+				mainPanelController.closeMessageWindow();
+
+				EventManager.getInstance().fireEvent(Event.APPLICATION_INITIALISED);
+				isInitialised = true;
+			} catch (Exception e) {
+				log.error("Error initialising data", e);
+			}
+		});
+	}
+	
+	public void stop() {
+		log.info("Stopping application");
+
+		mediaManager.cleanUpResources();
+
+		if (isInitialised) {
+			settingsManager.saveWindowSettings(stage);
+			settingsManager.saveSettings();
+		}
+
+		try {
+			jettyServer.stop();
+		} catch (Exception e) {
+			log.error("Error shutting down application", e);
+		}
+	}
+}
