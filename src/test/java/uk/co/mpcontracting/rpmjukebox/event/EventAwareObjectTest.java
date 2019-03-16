1d09de8feb4d5e223cbeb675e7e5037391456d0c
Matt Parker <matt@mpcontracting.co.uk>
2019-03-16 12:09:32 +0000

    Event tests refactored

--- src/test/java/uk/co/mpcontracting/rpmjukebox/event/EventAwareObjectTest.java
+++ src/test/java/uk/co/mpcontracting/rpmjukebox/event/EventAwareObjectTest.java
@@ -1,15 +1,14 @@
 package uk.co.mpcontracting.rpmjukebox.event;
 
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import uk.co.mpcontracting.rpmjukebox.test.support.AbstractEventTest;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.Before;
-import org.junit.Test;
-
-import uk.co.mpcontracting.rpmjukebox.test.support.AbstractEventTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.co.mpcontracting.rpmjukebox.event.Event.TEST_EVENT;
 
 public class EventAwareObjectTest extends AbstractEventTest {
 
@@ -32,10 +31,10 @@ public class EventAwareObjectTest extends AbstractEventTest {
             }
         };
 
-        eventAwareObject.fireEvent(Event.TEST_EVENT);
+        eventAwareObject.fireEvent(TEST_EVENT);
 
         latch.await(2000, TimeUnit.MILLISECONDS);
 
-        assertThat("Received event should be " + Event.TEST_EVENT, receivedEvent, equalTo(Event.TEST_EVENT));
+        assertThat(receivedEvent).isEqualTo(TEST_EVENT);
     }
 }
--- src/test/java/uk/co/mpcontracting/rpmjukebox/event/EventManagerTest.java
+++ src/test/java/uk/co/mpcontracting/rpmjukebox/event/EventManagerTest.java
@@ -1,15 +1,14 @@
 package uk.co.mpcontracting.rpmjukebox.event;
 
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import uk.co.mpcontracting.rpmjukebox.test.support.AbstractEventTest;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.Before;
-import org.junit.Test;
-
-import uk.co.mpcontracting.rpmjukebox.test.support.AbstractEventTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.co.mpcontracting.rpmjukebox.event.Event.TEST_EVENT;
 
 public class EventManagerTest extends AbstractEventTest {
 
@@ -34,6 +33,6 @@ public class EventManagerTest extends AbstractEventTest {
 
         latch.await(2000, TimeUnit.MILLISECONDS);
 
-        assertThat("Received event should be " + Event.TEST_EVENT, receivedEvent, equalTo(Event.TEST_EVENT));
+        assertThat(receivedEvent).isEqualTo(TEST_EVENT);
     }
 }
