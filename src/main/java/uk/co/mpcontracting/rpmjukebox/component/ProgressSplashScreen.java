9dd78f1cb6728c88205981cb38ccfa35f8b102a2
Matt Parker <matt@mpcontracting.co.uk>
2020-07-20 23:21:58 +0100

    Version 5.0.0 : Fixed splash screen styling

--- src/main/java/uk/co/mpcontracting/rpmjukebox/component/ProgressSplashScreen.java
+++ src/main/java/uk/co/mpcontracting/rpmjukebox/component/ProgressSplashScreen.java
@@ -3,7 +3,7 @@ package uk.co.mpcontracting.rpmjukebox.component;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
 import lombok.extern.slf4j.Slf4j;
 import uk.co.mpcontracting.rpmjukebox.javafx.SplashScreen;
 import uk.co.mpcontracting.rpmjukebox.support.Constants;
@@ -25,10 +25,10 @@ public class ProgressSplashScreen extends SplashScreen implements Constants {
         progressLabel.setPrefHeight(22);
         progressLabel.setText(null);
 
-        VBox vbox = new VBox();
-        vbox.getChildren().addAll(imageView, progressLabel);
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().addAll(imageView, progressLabel);
 
-        return vbox;
+        return stackPane;
     }
 
     private String loadStyle() {
